6afaf50406796338ee5cc3d0e0873475d66de456
Shreshti <shreshti@vikramdamodar.home>
2023-01-11 21:52:52 +0000

    Added addition programme

---
+++ python/02-plus.py
@@ -0,0 +1,7 @@
+num1 = input ("Enter a number: ")
+num2 = input ("Enter another number: ")
+
+sum = (num1) + (num2)
+
+print(" The total of " + str(num1) + " and " + str(num2) + " is " + str(sum))
+
